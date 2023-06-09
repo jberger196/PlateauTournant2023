90d6ec9b9536821430b4d4e4440485621579851f
dduporte <123366526+dduporte@users.noreply.github.com>
2023-06-09 17:01:29 +0200

    Moteur Signalisation version finale

--- Signalisation.h
+++ Signalisation.h
@@ -10,17 +10,17 @@ class Signalisation
 public:
 	Signalisation(int pinVoyantVert, int pinVoyantJaune, int pinVoyantRouge);
 
-	void signalerChargeBatterie();
+	void signalerChargeBatterie(); //allumer vert constant, utilisé dans Ares::alimenterBatterie
 	void finSignalerChargeBatterie();
 
-	void signalerPbAlim();
+	void signalerPbAlim(); //jaune clignotant, PAS UTILISEE
 	void finSignalerPbAlim();
 	thread tSignalerPbAlim();
 
-	void signalerPbSecurite();
+	void signalerPbSecurite(); //allumer rouge constant, PAS UTILISEE
 	void finSignalerPbSecurite();
 
-	void signalerMiseEnMouvement();
+	void signalerMiseEnMouvement(); //allumage successif vert jaune rouge, utilisé dans PlateauTournant::tournerHoraire/tournerAntiHoraire
 	void finirMiseEnMouvement();
 
 	/*void arretUrgence();
